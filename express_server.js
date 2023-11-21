ee466e1aa7e3647eecfe299d18683df0eaf49c1d
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-11-21 15:23:07 -0500

    Add urls_new.ejs to submit urls

--- express_server.js
+++ express_server.js
@@ -5,6 +5,9 @@ const PORT = 8080; // default port 8080
 // Set ejs as the view engine
 app.set("view engine", "ejs");
 
+// convert the request body from a Buffer into string
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -21,6 +24,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
