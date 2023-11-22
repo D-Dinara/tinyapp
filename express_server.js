ad21c03867c52c2c6f7f054ea846b082c5aafc9a
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-11-22 14:09:05 -0500

    Modify _header.ejs to display a username using cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 // Set ejs as the view engine
 app.set("view engine", "ejs");
 
-// convert the request body from a Buffer into string
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // convert the request body from a Buffer into string
+app.use(cookieParser());
 
 // a function that returns a string of 6 random alphanumeric characters
 // implemeneted in order to simulate generating a "unique" Short URL id
@@ -38,13 +39,19 @@ app.get("/urls.json", (req, res) => {
 
 // render information about all URLs and their shortened forms
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 // render a page to create new short URLs
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 // the id-longURL key-value pair are saved to the urlDatabase when a POST request to /urls is received
@@ -56,7 +63,11 @@ app.post("/urls", (req, res) => {
 
 // render information about a single URL and its shortened form
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
