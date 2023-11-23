8f7d3eb67099e100e5b055ef98a5924c3f967b55
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-11-22 22:55:40 -0500

    Add user_registration.ejs

--- express_server.js
+++ express_server.js
@@ -106,6 +106,14 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+// render registration form
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("user_registration", templateVars);
+});
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
