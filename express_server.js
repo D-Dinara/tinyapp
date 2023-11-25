a12af3d9fc2f70fe7691bb7c043e947f2a30e3ba
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2023-11-24 23:08:03 -0500

    Implement basic permission features

--- express_server.js
+++ express_server.js
@@ -54,16 +54,30 @@ app.get("/urls/new", (req, res) => {
   const templateVars = {
     user: users[userId]
   };
-  res.render("urls_new", templateVars);
+  // check if user is logged in
+  // if logged in show the page to create a new short URL
+  if (users[userId]) {
+    res.render("urls_new", templateVars);
+    // if not logged in show the login form
+  } else {
+    res.redirect("/login");
+  }
 });
 
 // POST request to /urls saves the id-longURL key-value pair to the urlDatabase
 app.post("/urls", (req, res) => {
-  // generate short URL id
-  const id = generateRandomString();
-  // get user input and save to urlDatabase
-  urlDatabase[id] = req.body.longURL;
-  res.redirect(`/urls/${id}`);
+  const userId = req.cookies["user_id"];
+  // check if user is logged in
+  if (users[userId]) {
+    // generate short URL id
+    const id = generateRandomString();
+    // get user input and save to urlDatabase
+    urlDatabase[id] = req.body.longURL;
+    res.redirect(`/urls/${id}`);
+    // if not logged in show a message
+  } else {
+    res.status(403).send("You need to login to be able to shorten URLs\n");
+  }
 });
 
 // render information about a single URL and its shortened form
@@ -90,7 +104,7 @@ app.get("/u/:id", (req, res) => {
     res.redirect(longURL);
   } else {
     // handle the case where the short URL does not exist in the database
-    res.status(404).send("Short URL not found");
+    res.status(404).send("Short URL not found\n");
   }
 });
 
@@ -107,17 +121,17 @@ app.post("/login", (req, res) => {
 
   // check if email or password is empty
   if (!email || !password) {
-    return res.status(400).send("Email and password cannot be empty");
+    return res.status(400).send("Email and password cannot be empty\n");
   }
 
   const user = getUserByEmail(email); // find user in the users object
   // check if user exists
   if (!user) {
-    return res.status(403).send("Email is not registered");
+    return res.status(403).send("Email is not registered\n");
   } else {
     // check if the password match
     if (password !== user.password) {
-      return res.status(403).send("Password is invalid");
+      return res.status(403).send("Password is invalid\n");
     }
     // set user_id cookie
     res.cookie("user_id", user.userId);
@@ -138,7 +152,13 @@ app.get("/register", (req, res) => {
   const templateVars = {
     user: users[userId]
   };
-  res.render("user_registration", templateVars);
+  // check if user is logged in
+  if (users[userId]) {
+    res.redirect("/urls");
+    // if not logged in show the registration form
+  } else {
+    res.render("user_registration", templateVars);
+  }
 });
 
 // POST route for /register adds new user object to global users object, sets user_id cookie
@@ -149,12 +169,12 @@ app.post("/register", (req, res) => {
 
   // check if email or password is empty
   if (!email || !password) {
-    return res.status(400).send("Email and password cannot be empty");
+    return res.status(400).send("Email and password cannot be empty\n");
   }
 
   // check if the email is already registered
   if (getUserByEmail(email)) {
-    return res.status(400).send("Email already registered");
+    return res.status(400).send("Email already registered\n");
   }
   
   // generate user id
@@ -174,7 +194,13 @@ app.get("/login", (req, res) => {
   const templateVars = {
     user: users[userId]
   };
-  res.render("user_login", templateVars);
+  // check if user is logged in
+  if (users[userId]) {
+    res.redirect("/urls");
+    // if not logged in show the login form
+  } else {
+    res.render("user_login", templateVars);
+  }
 });
 
 
